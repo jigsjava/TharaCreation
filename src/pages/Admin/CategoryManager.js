f621765d5f6b035c50e0629650c04bc4a62d1072
jigsjava <141757860+jigsjava@users.noreply.github.com>
2024-06-29 19:52:24 +0530

    show data in table

--- src/pages/Admin/CategoryManager.js
+++ src/pages/Admin/CategoryManager.js
@@ -1,43 +1,96 @@
-import React, { useState } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React, { useEffect, useState } from 'react';
+import { Formik, Form as FormikForm } from 'formik';
 import * as Yup from 'yup';
 import './CategoryManager.scss';
 import { DeleteIcon, EditIcon, ViewIcon } from '../../assets/icons';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import SearchForm from '../../components/SearchForm';
+import { Form, Button,Modal } from 'react-bootstrap';
+import AxiosInstance from '../../helpers/AxiosRequest';
+import { toast } from 'react-toastify';
 
 const CategoryManager = () => {
   const [categories, setCategories] = useState([]);
   const [previewImage, setPreviewImage] = useState(null);
   const [show, setShow] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const initialValues = {
-    name: '',
-    image: null,
+  const fetchData = async() => {
+    try {
+      const response = await AxiosInstance.get('/category/getcategory');
+      setCategories(response?.data?.category);
+      toast.success('Data fetched successfully!');
+    } catch (error) {
+      toast.error('Error fetching data');
+    }
   };
 
-  const validationSchema = Yup.object({
-    name: Yup.string().required('Category name is required'),
-    image: Yup.mixed().required('Category image is required')
-      .test('fileType', 'Unsupported File Format', value => value && ['image/jpeg', 'image/png', 'image/gif'].includes(value.type)),
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleDelete = async (id) => {
+    console.log("id",id)
+    try {
+      const response = await AxiosInstance.delete(`category/deletecategory?id=${id}`);
+      if (response.status === 200) {
+        // toast.success('Category deleted successfully!');
+        fetchData();
+      }
+    } catch (error) {
+      toast.error('An error occurred during deletion');
+    }
+  };
+
+  const initialValues = {
+    categoryName: '',
+    image: null,
+  };
+  const validationSchema = Yup.object().shape({
+    categoryName: Yup.string().required('Category name is required'),
+    image: Yup.mixed().required('Image is required'),
   });
 
-  const onSubmit = (values, { resetForm }) => {
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      const newCategory = {
-        name: values.name,
-        imageUrl: reader.result,
-        active: values.active,
-      };
-      setCategories([...categories, newCategory]);
-      resetForm();
-    };
-    reader.readAsDataURL(values.image);
+  const handleSubmit = async(values, { resetForm, setErrors, setSubmitting }) => {
+    setIsLoading(true);
+    const formData = new FormData();
+   
+    formData.append('name', values.categoryName);
+    formData.append('categoryImages', values.image);
+
+    try {
+      const response = await AxiosInstance.post("/category/addcategory", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success("Category added successfully", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        fetchData();
+        resetForm();
+      }
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        toast.error(error.response.data.error, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        setErrors(error.response.data.errors || {});
+      } else {
+        toast.error("An unexpected error occurred.", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      }
+    } finally {
+      setSubmitting(false);
+      setIsLoading(false);
+    }
+  
   };
 
   return (
@@ -51,60 +104,55 @@ const CategoryManager = () => {
             <Formik
               initialValues={initialValues}
               validationSchema={validationSchema}
-              onSubmit={onSubmit}
+              onSubmit={handleSubmit}
             >
-              {({ setFieldValue }) => (
-                <Form className="category-form">
-                  <div className="form-group">
-                    <label htmlFor="name">Category Name</label>
-                    <Field type="text" id="name" name="name" />
-                    <ErrorMessage
-                      name="name"
-                      component="div"
-                      className="error-message"
+              {({
+                setFieldValue,
+                handleChange,
+                handleBlur,
+                values,
+                errors,
+                touched,
+              }) => (
+                <FormikForm>
+                  <Form.Group className="mb-3" controlId="categoryName">
+                    <Form.Label>Category Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="categoryName"
+                      value={values.categoryName}
+                      placeholder="Enter category name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
                     />
-                  </div>
+                    {touched.categoryName && errors.categoryName && (
+                      <Form.Text className="font16Red">
+                        {errors.categoryName}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
 
-                  <div className="form-group">
-                    <label htmlFor="image">Category Image</label>
-                    <input
-                      id="image"
-                      name="image"
+                  <Form.Group className="mb-3" controlId="image">
+                    <Form.Label>Image Upload</Form.Label>
+                    <Form.Control
                       type="file"
-                      onChange={(event) => {
-                        const file = event.currentTarget.files[0];
-                        setFieldValue("image", file);
-
-                        // Set preview image
-                        const reader = new FileReader();
-                        reader.onloadend = () => {
-                          setPreviewImage(reader.result);
-                        };
-                        reader.readAsDataURL(file);
-                      }}
-                    />
-                    <ErrorMessage
                       name="image"
-                      component="div"
-                      className="error-message"
+                      onChange={(event) => {
+                        setFieldValue("image", event.currentTarget.files[0]);
+                      }}
+                      onBlur={handleBlur}
                     />
-                  </div>
+                    {touched.image && errors.image && (
+                      <Form.Text className="font16Red">
+                        {errors.image}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
 
-                  {previewImage && (
-                    <div className="image-preview">
-                      <img
-                        src={previewImage}
-                        alt="Preview"
-                        height={100}
-                        width={100}
-                      />
-                    </div>
-                  )}
-
-                  <button type="submit" className="submit-button">
-                    Add Category
-                  </button>
-                </Form>
+                  <Button type="submit" disabled={isLoading}>
+                    {isLoading ? "Submitting..." : "Submit"}
+                  </Button>
+                </FormikForm>
               )}
             </Formik>
           </div>
@@ -127,60 +175,37 @@ const CategoryManager = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Attaa</td>
-              <td>image</td>
-              <td>
-                <span className="badge bg-success">Approved</span>
-              </td>
-              <td>
-                <ViewIcon />
-
-                <span onClick={handleShow}>
-                  <EditIcon />
-                </span>
-                <DeleteIcon />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Attaa</td>
-              <td>
-                <img
-                  src={previewImage}
-                  alt={previewImage}
-                  className="category-image"
-                  height={50}
-                  width={50}
-                />
-              </td>
-              <td>
-                <span className="badge bg-danger">Rejected</span>
-              </td>
-              <td>
-                <ViewIcon />
-                <span onClick={handleShow}>
-                  <EditIcon />
-                </span>
-                <DeleteIcon />
-              </td>
-            </tr>
-            <tr>
-              <th scope="row">1</th>
-              <td>Attaa</td>
-              <td>image</td>
-              <td>
-                <span className="badge bg-secondary">Pending</span>
-              </td>
-              <td>
-                <ViewIcon />
-                <span onClick={handleShow}>
-                  <EditIcon />
-                </span>
-                <DeleteIcon />
-              </td>
-            </tr>
+            {categories &&
+              categories.map((category, index) => {
+                const { name, images,id} = category;
+                return (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{name}</td>
+                    <td>
+                      {images &&
+                        images.map((image, index) => (
+                          <img
+                            key={index}
+                            src={image}
+                            alt={category.name}
+                            style={{ width: "50px", height: "auto",padding:'5px' }}
+                          />
+                        ))}
+                    </td>
+                    <td>
+                      <span className="badge bg-success">Approved</span>
+                    </td>
+                    <td>
+                      <ViewIcon />
+                      <span onClick={handleShow}>
+                        <EditIcon />
+                      </span>
+                      <span style={{cursor:'pointer'}} onClick={() => handleDelete(id)}><DeleteIcon/></span>
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
@@ -195,10 +220,48 @@ const CategoryManager = () => {
           <Modal.Title>Add New Category</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Formik
+         
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default CategoryManager;
+
+
+{/* <tr>
+<th scope="row">2</th>
+<td>Attaa</td>
+<td>
+  <img
+    src={previewImage}
+    alt={previewImage}
+    className="category-image"
+    height={50}
+    width={50}
+  />
+</td>
+<td>
+<span className="badge bg-success">Approved</span>  <span className="badge bg-danger">Rejected</span> <span className="badge bg-secondary">Pending</span>
+</td>
+<td>
+  <ViewIcon />
+  <span onClick={handleShow}>
+    <EditIcon />
+  </span>
+  <DeleteIcon />
+</td>
+</tr> */}
+ {/* <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
-            onSubmit={onSubmit}
+            onSubmit={handleSubmit}
           >
             {({ setFieldValue }) => (
               <Form className="category-form">
@@ -254,16 +317,4 @@ const CategoryManager = () => {
                 </button>
               </Form>
             )}
-          </Formik>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-  );
-};
-
-export default CategoryManager;
+          </Formik> */}
