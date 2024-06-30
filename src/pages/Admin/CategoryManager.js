0b0c01219f41c867c76bae7e732cdaa30ad81d12
jigsjava <141757860+jigsjava@users.noreply.github.com>
2024-06-30 14:25:17 +0530

    view buttom

--- src/pages/Admin/CategoryManager.js
+++ src/pages/Admin/CategoryManager.js
@@ -4,25 +4,51 @@ import * as Yup from 'yup';
 import './CategoryManager.scss';
 import { DeleteIcon, EditIcon, ViewIcon } from '../../assets/icons';
 import SearchForm from '../../components/SearchForm';
-import { Form, Button,Modal } from 'react-bootstrap';
+import { Form, Button, Modal, Spinner } from 'react-bootstrap';
 import AxiosInstance from '../../helpers/AxiosRequest';
 import { toast } from 'react-toastify';
 
 const CategoryManager = () => {
   const [categories, setCategories] = useState([]);
-  const [previewImage, setPreviewImage] = useState(null);
   const [show, setShow] = useState(false);
+  const [showCategory, setShowCategory] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCategoryId, setCurrentCategoryId] = useState(null);
+  const [getCategory, setGetCategory] = useState(null)
 
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const getCategoryDetails = async(id) => {
+    try {
+      const response = await AxiosInstance.get(`category/getcategorybyid?id=${id}`);
+      if (response.status === 200) {
+        setGetCategory(response?.data?.category)
+      }
+    } catch (error) {
+      toast.error('An error occurred during deletion');
+    }
+  }
 
+  const handleClose = () => {
+    setShow(false);
+    setCurrentCategoryId(null);
+  }
+  const handleShow = (id) => {
+    setShow(true);
+    setCurrentCategoryId(id)
+  }
+
+  const handleCloseShowCateogry = () => {
+    setShowCategory(false);
+  }
+  const handleShowCategory = (id) => {
+    setShowCategory(true);
+    getCategoryDetails(id)
+  }
   const fetchData = async() => {
     try {
       const response = await AxiosInstance.get('/category/getcategory');
       setCategories(response?.data?.category);
-      toast.success('Data fetched successfully!');
+      // toast.success('Data fetched successfully!');
     } catch (error) {
       toast.error('Error fetching data');
     }
@@ -33,7 +59,6 @@ const CategoryManager = () => {
   }, []);
 
   const handleDelete = async (id) => {
-    console.log("id",id)
     try {
       const response = await AxiosInstance.delete(`category/deletecategory?id=${id}`);
       if (response.status === 200) {
@@ -58,7 +83,7 @@ const CategoryManager = () => {
     setIsLoading(true);
     const formData = new FormData();
    
-    formData.append('name', values.categoryName);
+    formData.append('categoryName', values.categoryName);
     formData.append('categoryImages', values.image);
 
     try {
@@ -90,9 +115,50 @@ const CategoryManager = () => {
       setSubmitting(false);
       setIsLoading(false);
     }
+  };
   
+  const handleUpdateSubmit = async (values, { setErrors, setSubmitting,resetForm }) => {
+ 
+    setIsLoading(true);
+    const formData = new FormData();
+    formData.append('categoryName', values.categoryName);
+    if (values.image) {
+      formData.append('categoryImages', values.image);
+    }
+
+    try {
+      const response = await AxiosInstance.post(`/category/updatecategory?id=${currentCategoryId}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (response.status === 200) {
+        toast.success("Category updated successfully", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        fetchData();
+        resetForm();
+        handleClose();
+      }
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.error) {
+        toast.error(error.response.data.error, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+        setErrors(error.response.data.errors || {});
+      } else {
+        toast.error("An unexpected error occurred.", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      }
+    } finally {
+      setSubmitting(false);
+      setIsLoading(false);
+    }
   };
 
+  console.log("first",getCategory)
   return (
     <div className="category-manager">
       <div
@@ -150,7 +216,14 @@ const CategoryManager = () => {
                   </Form.Group>
 
                   <Button type="submit" disabled={isLoading}>
-                    {isLoading ? "Submitting..." : "Submit"}
+                    {isLoading ? (
+                      <>
+                        <Spinner animation="border" size="sm me-2" />
+                        Adding ...
+                      </>
+                    ) : (
+                      "Add New Category"
+                    )}
                   </Button>
                 </FormikForm>
               )}
@@ -177,19 +250,23 @@ const CategoryManager = () => {
           <tbody>
             {categories &&
               categories.map((category, index) => {
-                const { name, images,id} = category;
+                const { categoryName, images, id } = category;
                 return (
                   <tr key={index}>
                     <th scope="row">{index + 1}</th>
-                    <td>{name}</td>
+                    <td>{categoryName}</td>
                     <td>
                       {images &&
                         images.map((image, index) => (
                           <img
                             key={index}
                             src={image}
-                            alt={category.name}
-                            style={{ width: "50px", height: "auto",padding:'5px' }}
+                            alt={category.categoryName}
+                            style={{
+                              width: "50px",
+                              height: "auto",
+                              padding: "5px",
+                            }}
                           />
                         ))}
                     </td>
@@ -197,11 +274,24 @@ const CategoryManager = () => {
                       <span className="badge bg-success">Approved</span>
                     </td>
                     <td>
-                      <ViewIcon />
-                      <span onClick={handleShow}>
+                      <span
+                        onClick={() => handleShowCategory(id)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        <ViewIcon />
+                      </span>
+                      <span
+                        onClick={() => handleShow(id)}
+                        style={{ cursor: "pointer" }}
+                      >
                         <EditIcon />
                       </span>
-                      <span style={{cursor:'pointer'}} onClick={() => handleDelete(id)}><DeleteIcon/></span>
+                      <span
+                        style={{ cursor: "pointer" }}
+                        onClick={() => handleDelete(id)}
+                      >
+                        <DeleteIcon />
+                      </span>
                     </td>
                   </tr>
                 );
@@ -220,10 +310,106 @@ const CategoryManager = () => {
           <Modal.Title>Add New Category</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-         
+          <Formik
+            initialValues={{
+              categoryName: "",
+              image: null,
+            }}
+            validationSchema={Yup.object().shape({
+              categoryName: Yup.string().required("Category name is required"),
+              image: Yup.mixed(),
+            })}
+            onSubmit={handleUpdateSubmit}
+            enableReinitialize
+          >
+            {({
+              setFieldValue,
+              handleChange,
+              handleBlur,
+              values,
+              errors,
+              touched,
+            }) => (
+              <FormikForm>
+                <Form.Group className="mb-3" controlId="categoryName">
+                  <Form.Label>Category Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="categoryName"
+                    value={values.categoryName}
+                    placeholder="Enter category name"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.categoryName && errors.categoryName && (
+                    <Form.Text className="font16Red">
+                      {errors.categoryName}
+                    </Form.Text>
+                  )}
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="image">
+                  <Form.Label>Image Upload</Form.Label>
+                  <Form.Control
+                    type="file"
+                    name="image"
+                    onChange={(event) => {
+                      setFieldValue("image", event.currentTarget.files[0]);
+                    }}
+                    onBlur={handleBlur}
+                  />
+                  {touched.image && errors.image && (
+                    <Form.Text className="font16Red">{errors.image}</Form.Text>
+                  )}
+                </Form.Group>
+
+                <Button type="submit" disabled={isLoading}>
+                  {isLoading ? (
+                    <>
+                      <Spinner animation="border" size="sm me-2" /> Updating...
+                    </>
+                  ) : (
+                    "Update Category"
+                  )}
+                </Button>
+              </FormikForm>
+            )}
+          </Formik>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal
+        show={showCategory}
+        onHide={handleCloseShowCateogry}
+        animation={false}
+        centered
+        size="md"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Your Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='d-flex justify-content-center align-items-center flex-column'>
+            <h3>{getCategory.categoryName}</h3>
+            <div>
+              {getCategory.images &&
+                getCategory.images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={getCategory.categoryName}
+                    style={{ width: "200px", height: "auto", margin: "10px 0" }}
+                  />
+                ))}
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseShowCateogry}>
             Close
           </Button>
         </Modal.Footer>
